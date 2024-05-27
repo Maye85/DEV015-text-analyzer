4ea75a473081cb327e50c6df9482ac7192b8da6f
Maye85 <mayegabrielaboffil@gmail.com>
2024-05-27 15:27:14 -0500

    comencé con el a declarar un arreglo para hacer el for

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ const palabras = document.querySelector('.lista .palabras');
 textarea.addEventListener("input", () => {
     const text = textarea.value;
     const wordCount = analyzer.getWordCount(text);
-    palabras.textContent = `PALABRAS: ${wordCount}`;
+    palabras.textContent = `PALABRAS: ${wordCount}`; 
 });
+
+const sinEspacios = [".", ",", ":", ";", "...", "¿?", "¡!"];
+for (let)
 
 const resetButton = document.getElementById('reset-button');
  console.log(resetButton);
 resetButton.addEventListener('click', () => {
     textarea.value = ''; 
-    
+    palabras.textContent="PALABRAS: 0"
 });
