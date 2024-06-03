60f62bc4eab97ee939ae5e3fe5aedb3eec706708
Maye85 <mayegabrielaboffil@gmail.com>
2024-06-03 13:05:40 -0500

    funciones del analyzer listas y test individuales correctos

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,29 +1,33 @@
 const analyzer = {  
 
-  getWordCount: (text) => {
-    if(!text) return 0;
+  getWordCount: (text) => { //cuenta el numero de palabras en el texto
+    if(!text) return 0; //verificamos si el texto está vacio, si está vacio da 0
     return text.trim().split(/\s+/).length;
+    //text.trim elimina espacios del inicio y fin, con el .split(/\s+/) cuenta por espacios
   },
 
-  getCharacterCount: (text) => {
+  getCharacterCount: (text) => { //cuenta el numero de caracteres en el texto
     if(!text) return 0;
-    return text.length;
+    return text.length;//devuelve el número de caracteres
   },
 
 
   getCharacterCountExcludingSpaces: (text) => {
-     if(!text) return 0;
-     const signosDePuntuacion = [".", ",", ":", ";", "¿", "?", "¡", "!"];
-     let charCountExSpa = 0;
-
-     for (let i = 0; i < text.length; i++) {
-      const char = text[i];
+    if(!text) return 0;
+    const signosDePuntuacion = [".", ",", ":", ";", "¿", "?", "¡", "!"];
+    //creamos la constante con los signos de puntuacion correspondientes
+    let charCountExSpa = 0;
+    //creamos una variable que estará en 0
+    for (let i = 0; i < text.length; i++) {
+      //bucle para contar el indice desde 0 y si es mayor que 0 ir sumando 1
+      const char = text[i]; //se accede al indice yse evalua si cumple las condiciones del if
       if (!signosDePuntuacion.includes(char) && char !==' '){
-          charCountExSpa++;
+      // evalua que en la cadena no haya espacios ni los signos
+        charCountExSpa++;
       }
-     }
-      return charCountExSpa;
-    },
+    }
+    return charCountExSpa;
+  },
   
   getAverageWordLength: (text) => {    
     if(!text || text.trim().length === 0) return 0; // verifica si el texto está vacio
@@ -39,12 +43,15 @@ const analyzer = {
 
   getNumberCount: (text) => {
     if(!text) return 0;
-    const todosLosNumeros = /\d/g; //Define una expresión regular para encontrar todos los dígitos (del 0 al 9)
-    const numbers = text.match(todosLosNumeros); //Buscar todos los dígitos en el texto
+    const todosLosNumeros = /\d/g; 
+    //Define una expresión regular para encontrar todos los dígitos (del 0 al 9)
+    const numbers = text.match(todosLosNumeros); 
+    //Buscar todos los dígitos que coincidan de la const todosLosNumeros 
     if (!numbers) return 0;
+    //sino hay numeros arroja 0
 
     return numbers.length;
-
+    
   },
 
 
@@ -55,7 +62,8 @@ const analyzer = {
      
     if (!number) return 0;
     const sum = number.reduce((acc, num) => acc + parseInt(num, 10), 0);
-
+    //con reduce iteramos y toma el valor acumulado y el numero actual y se suma 
+    //luego de convertirlo en un valor entero
     return sum;
 
 
--- src/index.js
+++ src/index.js
@@ -2,48 +2,48 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
-const textarea = document.querySelector("textarea");
-const palabras = document.querySelector('.lista .palabras');
-const caracteresSinEspacios = document.querySelector('.lista .sin-espacios');
-const caracteres = document.querySelector('.lista .caracteres');
-const longitud = document.querySelector('.lista .longitud');
-const numeros = document.querySelector('.lista .numeros');
-const suma = document.querySelector('.lista .suma')
-const resetButton = document.getElementById('reset-button');
+const textarea = document.querySelector("textarea"); //busco el elemento testarea
+const palabras = document.querySelector("li[data-testid='word-count']");
+const caracteresSinEspacios = document.querySelector("li[data-testid='character-count']");
+const caracteres = document.querySelector("li[data-testid='character-no-spaces-count']");
+const longitud = document.querySelector("li[data-testid='number-count']");
+const numeros = document.querySelector("li[data-testid='number-sum']");
+const suma = document.querySelector("li[data-testid='word-legth-average']");
+const resetButton = document.getElementById('reset-button'); //busco el elemento id reset-button
 
-textarea.addEventListener("input", () => {
- const text = textarea.value;
+textarea.addEventListener("input", () => { // escucha el input cada que se escribe
+const text = textarea.value; //declaramos la constante y obtiene el contenido act del cuadro de texto
 
- const wordCount = analyzer.getWordCount(text);
- palabras.textContent = `PALABRAS: ${wordCount}`; 
+const wordCount = analyzer.getWordCount(text); //llama a una funcion para contar el numero de palabras
+palabras.textContent = `PALABRAS: ${wordCount}`; //crea cadena de texto con el numero de palabras
 
- const characterCount = analyzer.getCharacterCount(text);
- caracteres.textContent = `CARACTERES: ${characterCount}`;
+ const characterCount = analyzer.getCharacterCount(text);//llama a una funcion para contar el numero de caracteres
+ caracteres.textContent = `CARACTERES: ${characterCount}`;//crea cadena de texto con el numero de caracteres
 
- const characterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(text);
- caracteresSinEspacios.textContent = `CARACTERES SIN ESPACIOS: ${characterCountExcludingSpaces}`;
+ const characterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(text);//llama a una funcion para contar el numero de palabras sin epacios ni signos de puntuacion
+ caracteresSinEspacios.textContent = `CARACTERES SIN ESPACIOS: ${characterCountExcludingSpaces}`;//crea cadena de texto con el numero de palabras sin espacios ni signos de puntuacion
 
- const averageWordLength = analyzer.getAverageWordLength(text);
- longitud.textContent = `LONGITUD: ${averageWordLength}`;
+ const averageWordLength = analyzer.getAverageWordLength(text);//llama a una funcion para contar la longitud de palabras
+ longitud.textContent = `LONGITUD: ${averageWordLength}`;//crea cadena de texto con la longitud de palabras
 
- const numberCount = analyzer.getNumberCount(text);
- numeros.textContent = `NUMEROS: ${numberCount}`;
+ const numberCount = analyzer.getNumberCount(text);//llama a una funcion para contar los números
+ numeros.textContent = `NUMEROS: ${numberCount}`;//crea cadena de texto con la cantidad de números
 
- const numberSum = analyzer.getNumberSum(text);
- suma.textContent = `SUMA: ${numberSum}`;
+ const numberSum = analyzer.getNumberSum(text);//llama a una funcion para sumar los números
+ suma.textContent = `SUMA: ${numberSum}`; // //crea cadena de texto con la suma de números
+
 
 });
 
 
 
-resetButton.addEventListener('click', () => {
- textarea.value = ''; 
+resetButton.addEventListener('click', () => { // cuando se haga click se ejecutara la lista de funciones
+ textarea.value = ''; //borrará lo escrito en el textarea
  palabras.textContent="PALABRAS: 0";
  caracteres.textContent="CARACTERES: 0";
  caracteresSinEspacios.textContent = "CARACTERES SIN ESPACIOS: 0";
  longitud.textContent = "LONGITUD: 0";
  numeros.textContent = "NÚMEROS: 0";
  suma.textContent = "SUMA: 0";
-
-
+//dejará los númeos a 0 de cada una de la lista 
 })
